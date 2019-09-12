f1e34da3229591bc467247c2e0a88c3f361d8974
subinHu <490101299@qq.com>
2019-09-12 15:28:17 +0800

    feat: add common validator

--- packages/util/src/validate/validate.spec.ts
+++ packages/util/src/validate/validate.spec.ts
@@ -1,5 +1,5 @@
 import { FormControl } from '@angular/forms';
-import { isDecimal, isIdCard, isInt, isMobile, isNum, isUrl } from './validate';
+import { isDecimal, isIdCard, isInt, isMobile, isNum, isUrl, isCreditCard, isBase64, isEmail, isUUID, isTelPhone } from './validate';
 import { _Validators } from './validators';
 
 describe('utils: validate', () => {
@@ -95,6 +95,28 @@ describe('utils: validate', () => {
     }
   });
 
+  it('#isTelPhone', () => {
+    const data = [
+      { k: '15900000000', v: true },
+      { w: 'tel', k: '0757-88888888', v: true },
+      { w: 'tel', k: '07572-88888888', v: false },
+      { w: 'phone', k: '0757-88888888', v: false },
+      { w: 'multFormat', k: '14700000000/1154879879', v: true },
+      { k: '1590000000', v: false },
+      { k: '+8615900000000', v: true },
+    ];
+    for (const item of data) {
+      expect(isTelPhone(item.w, item.k)).toBe(item.v, `${item.k}=${typeof item.k} must be ${item.v}`);
+
+      const ctr = new FormControl(item.k);
+      if (item.v) {
+        expect(_Validators.telPhone(item.w)(ctr)).toBeNull();
+      } else {
+        expect(_Validators.telPhone(item.w)(ctr)).toEqual({ telPhone: true });
+      }
+    }
+  });
+
   it('#isUrl', () => {
     const data = [
       { k: 'http://1ziton.com', v: true },
@@ -111,6 +133,76 @@ describe('utils: validate', () => {
         expect(_Validators.url(ctr)).toBeNull();
       } else {
         expect(_Validators.url(ctr)).toEqual({ url: true });
+      }
+    }
+  });
+
+  it('#isBase64', () => {
+    const data = [
+      { k: 'data:image/jpeg;base64,/9j/2wCEAAIBAQEBAQIBAQIDAgECAwMCAgICAwMDAwMDAwMFAwQEBAQDBQUFBgYGBQUHBwgIBwcKCgoKCgwMDAwM', v: true },
+      { k: 'http://fex.1ziton.com/pixelmon', v: false },
+      { k: '//1ziton.com', v: false },
+    ];
+    for (const item of data) {
+      expect(isBase64(item.k)).toBe(item.v, `${item.k}=${typeof item.k} must be ${item.v}`);
+
+      const ctr = new FormControl(item.k);
+      if (item.v) {
+        expect(_Validators.base64(ctr)).toBeNull();
+      } else {
+        expect(_Validators.base64(ctr)).toEqual({ base64: true });
+      }
+    }
+  });
+
+  it('#isCreditCard', () => {
+    const data = [
+      { k: '6221888200604489572', v: true },
+      { k: '62218882006044895720', v: false },
+      { k: '622188820060448957', v: false },
+      { k: '6221-8882-0060-4489-572', v: true },
+    ];
+    for (const item of data) {
+      expect(isCreditCard(item.k)).toBe(item.v, `${item.k}=${typeof item.k} must be ${item.v}`);
+
+      const ctr = new FormControl(item.k);
+      if (item.v) {
+        expect(_Validators.creditCard(ctr)).toBeNull();
+      } else {
+        expect(_Validators.creditCard(ctr)).toEqual({ creditCard: true });
+      }
+    }
+  });
+
+  it('#isEmail', () => {
+    const data = [{ k: 'aaa@163.com', v: true }, { k: 'aaa163.com', v: false }, { k: 'aaa@163', v: false }, { k: '@163.com', v: false }];
+    for (const item of data) {
+      expect(isEmail(item.k)).toBe(item.v, `${item.k}=${typeof item.k} must be ${item.v}`);
+
+      const ctr = new FormControl(item.k);
+      if (item.v) {
+        expect(_Validators.email(ctr)).toBeNull();
+      } else {
+        expect(_Validators.email(ctr)).toEqual({ email: true });
+      }
+    }
+  });
+
+  it('#isUUID', () => {
+    const data = [
+      { k: '9d1a1f65-6099-7a13-83eb-c462a8ffcad2', v: true },
+      { k: '9d1a1f65 6099 7a13 83eb c462a8ffcad2', v: false },
+      { k: '9d1a1f6560997a1383ebc462a8ffcad2', v: false },
+      { k: '9d1a1f65-60997a1383ebc462a8ffcad2', v: false },
+    ];
+    for (const item of data) {
+      expect(isUUID(item.k)).toBe(item.v, `${item.k}=${typeof item.k} must be ${item.v}`);
+
+      const ctr = new FormControl(item.k);
+      if (item.v) {
+        expect(_Validators.uuid(ctr)).toBeNull();
+      } else {
+        expect(_Validators.uuid(ctr)).toEqual({ uuid: true });
       }
     }
   });
--- packages/util/src/validate/validate.ts
+++ packages/util/src/validate/validate.ts
@@ -24,9 +24,40 @@ export function isMobile(value: string): boolean {
   return typeof value === 'string' && /^(0|\+?86|17951)?(13[0-9]|15[0-9]|17[0678]|18[0-9]|14[57])[0-9]{8}$/.test(value);
 }
 
+/** 是否为手机号/座机/或有特殊符号分隔的电话号码 */
+export function isTelPhone(which = 'phone', value: string): boolean {
+  const _regex = {
+    phone: /^(0|\+?86|17951)?(13[0-9]|15[0-9]|17[0678]|18[0-9]|14[57])[0-9]{8}$/,
+    tel: /^[0]\d{2,3}-\d{7,8}$/, // 标准座机没分号
+    multFormat: /^\d[\s+-\/,，0-9]{0,34}$/, // 任意数字加特殊符号
+  };
+  return _regex[which].test(value);
+}
+
 /** 是否URL地址 */
 export function isUrl(url: string): boolean {
   return /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/.test(
     url,
   );
+}
+
+/** 是否base64编码 */
+export function isBase64(value: string): boolean {
+  return /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\/?%\s]*?)\s*$/i.test(value);
+}
+
+/** 是否银行卡格式 */
+export function isCreditCard(value: string): boolean {
+  const sanitized = value.replace(/[^0-9]+/g, '');
+  return /^(?:[1-9]{1})(?:\d{15}|\d{18})$/.test(sanitized);
+}
+
+/** 是否email */
+export function isEmail(value: string): boolean {
+  return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(value);
+}
+
+/** 是否uuid*/
+export function isUUID(value: string): boolean {
+  return /^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$/i.test(value);
 }
