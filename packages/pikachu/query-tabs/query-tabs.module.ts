e93d17a16d53b45355b59883c237a327ae765526
zomixi <zomixi@qq.com>
2019-11-22 18:32:56 +0800

    refactor(pikachu/query-tabs): standardizing

--- packages/pikachu/query-tabs/query-tabs.module.ts
+++ packages/pikachu/query-tabs/query-tabs.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FilterPipeModule, TranslatePipeModule } from '@pixelmon/theme';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NzTabsModule, NzPopoverModule, NzIconModule } from 'ng-zorro-antd';
 import { QueryTabsComponent } from './query-tabs.component';
 
 @NgModule({
   declarations: [QueryTabsComponent],
-  imports: [CommonModule, TranslatePipeModule, FilterPipeModule, NgZorroAntdModule],
-  exports: [CommonModule, TranslatePipeModule, FilterPipeModule, NgZorroAntdModule, QueryTabsComponent],
+  imports: [CommonModule, TranslatePipeModule, FilterPipeModule, NzTabsModule, NzPopoverModule, NzIconModule],
+  exports: [QueryTabsComponent],
 })
 export class QueryTabsModule {}
