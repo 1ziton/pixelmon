5b3c7eb6c8de8bc8284260aa0fb34672c72d67da
zomixi <zomixi@qq.com>
2019-11-25 14:57:19 +0800

    refactor(pikachu:query-tabs): use with smart-text

--- packages/pikachu/query-tabs/query-tabs.module.ts
+++ packages/pikachu/query-tabs/query-tabs.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FilterPipeModule, TranslatePipeModule } from '@pixelmon/theme';
 import { NzTabsModule, NzPopoverModule, NzIconModule } from 'ng-zorro-antd';
+import { SmartTextModule } from '../smart-text';
 import { QueryTabsComponent } from './query-tabs.component';
 
 @NgModule({
   declarations: [QueryTabsComponent],
-  imports: [CommonModule, TranslatePipeModule, FilterPipeModule, NzTabsModule, NzPopoverModule, NzIconModule],
+  imports: [CommonModule, TranslatePipeModule, FilterPipeModule, NzTabsModule, NzPopoverModule, NzIconModule, SmartTextModule],
   exports: [QueryTabsComponent],
 })
 export class QueryTabsModule {}
